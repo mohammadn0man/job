d17b365c6f446b5d0025ae93afb65fa0d817546e
Mohammad Noman <mohammad.noman@live.in>
2019-07-06 14:16:43 +0530

    header navigation bar splitted

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+require_once('header.php');
+?>
 <!DOCTYPE html>
 <html lang="en" xmlns:height="http://www.w3.org/1999/xhtml" xmlns:width="http://www.w3.org/1999/xhtml">
 <head>
@@ -17,31 +20,7 @@
 </head>
 <body>
     <!--navbar section-->
-    <header>
-        <nav id="navbar" class="navbar navbar-expand-md shadow rounded navbar-light fixed-top navbar-toggleable-sm" data-toggle="affix">
-            <a href="index.html" class="navbar-brand">
-                <img class="rounded" src="img/front logo.png" height="65" alt="StanIndia">
-            </a>
-            <button class="navbar-toggler" data-toggle="collapse"data-target="#navbar-collapse" aria-expanded="false" aria-controls="navbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbar-collapse">
-                <div class="navbar-nav ml-auto">
-                    <a href="#" class="nav-item nav-link active">Home</a>
-                    <div class="dropdown">
-                        <a href="#" class="nav-item nav-link btn-custom dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Products</a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                            <a class="nav-item nav-link dropdown-item" href="#">Mens</a>
-                            <a class="nav-item nav-link dropdown-item" href="#">Women</a>
-                        </div>
-                    </div>
-                    <a href="#" class="nav-item nav-link">Franchise</a>
-                    <a href="#" class="nav-item nav-link">Contact Us</a>
-                    <a href="#" class="nav-item nav-link">About Us</a>
-                </div>
-            </div>
-        </nav>
-    </header>
+<!--    at the top-->
 
     <!--carousel section-->
     <div id="carouselExampleIndicators" class="carousel shadow slide" data-ride="carousel">
